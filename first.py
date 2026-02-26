774abc9c757f8f364dfa1dd7c449280a20e1cea5
nigamchovatiya <nigam.techforceglobal@gmail.com>
2026-02-26 16:24:48 +0530

    data types

--- first.py
+++ first.py
@@ -11,3 +11,20 @@ currentdate = datetime.now()
 
 print("hello my name is " + name + " and I am " + age + " years old.")
 print("Today date is: ",currentdate)
+
+
+#data types
+x = 1
+print(x, type(x))
+
+x = "nigam"
+print(x, type(x))
+
+x = True
+print(x, type(x))
+
+x = 5.0
+print(x, type(x))
+
+x = None
+print(x, type(x))

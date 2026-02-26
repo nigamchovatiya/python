83f0230161f4a85a9016e51ecda444fd8292681e
nigamchovatiya <nigam.techforceglobal@gmail.com>
2026-02-26 14:52:09 +0530

    git init

---
+++ first.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+
+# print("hello")
+# print("Nigam Chovatiya")
+# print("I am a 21 year old.")
+
+
+name = "Nigam"
+age = "21"
+currentdate = datetime.now()
+
+print("hello my name is " + name + " and I am " + age + " years old.")
+print("Today date is: ",currentdate)
